5027bf875e128c8405884889962023ebc8aacdc9
styven <styvenn16@gmail.com>
2025-02-22 17:41:11 -0300

    fim

--- src/main/routes/subs.py
+++ src/main/routes/subs.py
@@ -3,6 +3,7 @@ from src.http_types.http_request import HttpRequest
 from src.validators.subscribers_creator_validator import subscribers_creator_validator
 from src.model.repositories.subscribers_repository import SubscribersRepository
 from src.controllers.subscribers.subscribers_creator import SubscribersCreator
+from src.controllers.subscribers.subscribers_manager import SubscriberManager
 subs_route_bp = Blueprint("subs_route", __name__)
 
 
@@ -14,5 +15,30 @@ def create_new_subs():
     subs_repo = SubscribersRepository()
     subs_creator = SubscribersCreator(subs_repo)
     http_response = subs_creator.create(http_request)
+
+    return jsonify(http_response.body), http_response.status_code
+
+
+
+@subs_route_bp.route("/subs/link/<link>/event/<event_id>", methods=["GET"])
+def subscribers_by_link(link, event_id):
+    subs_repo = SubscribersRepository()
+    subs_manager = SubscriberManager(subs_repo)
+
+    http_request = HttpRequest(param={ "link": link, "event_id": event_id })
+
+    http_response = subs_manager.get_subscribers_by_link(http_request)
+
+    return jsonify(http_response.body), http_response.status_code
+
+
+@subs_route_bp.route("/subs/ranking/event/<event_id>", methods=["GET"])
+def link_ranking(event_id):
+    subs_repo = SubscribersRepository()
+    subs_manager = SubscriberManager(subs_repo)
+
+    http_request = HttpRequest(param={ "event_id": event_id })
+
+    http_response = subs_manager.get_event_ranking(http_request)
 
     return jsonify(http_response.body), http_response.status_code
